2ff32b9dc32c4ecde35771e1c18cf264e121ff51
Rafal Wojdyla <rav@spotify.com>
2018-07-18 13:20:24 -0400

    codez

---
+++ src/main/java/sh/rav/TestTF.java
@@ -0,0 +1,23 @@
+package sh.rav;
+
+import org.tensorflow.Tensor;
+import org.tensorflow.Tensors;
+import org.tensorflow.example.Example;
+
+public class TestTF {
+
+    static Tensor<?> createTensor(){
+        Example e = Example.newBuilder().build();
+        return Tensors.create(e.toByteArray());
+    }
+
+    public static void main( String[] args ) {
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                public void run() {
+                    createTensor();
+                }
+            }).start();
+        }
+    }
+}
